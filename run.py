bbdc2b529e03514e4b9767f5ba2b468e45068f42
oqwa <oqwa@inbox.lv>
2018-02-10 19:50:37 +0300

    The ability to switch to https mode was added

--- run.py
+++ run.py
@@ -1,5 +1,12 @@
+import ssl
+
 from app.app import app
 from app.data.settings import *
 
 if __name__ == '__main__':
-    app.run(host=API_HOST, port=API_PORT)
+    if API_SSL:
+        ctx = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+        ctx.load_cert_chain(*API_SSL)
+    else:
+        ctx = None
+    app.run(host=API_HOST, port=API_PORT, ssl_context=ctx)
